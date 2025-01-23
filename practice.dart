7115f7b2b829638e97262cf51056063c8841aced
Sim Shad <samsudeenashad@icloud.com>
2025-01-23 12:00:42 +0530

    Create practice.dart

---
+++ practice.dart
@@ -0,0 +1,30 @@
+void (main)
+
+List <int> numbers = [1,2,3,4,5]
+
+for (int i in number) {
+	print(i);
+}
+
+String result = greet();
+
+print(result);
+
+print(greet())
+
+int age = displayAge(20);
+print("my age is $age");
+
+}
+
+String greet()=> "hello world";
+
+int displayAge (int age) => age;
+
+Map<String, Daynamic> person = {
+	" name" : "tom",
+	"age" :25
+    "isMale" : true
+};
+
+print(person )
